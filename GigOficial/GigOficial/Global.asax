34958c06f93f94b15dccb06aab310dbea8644dd7
Dani Llanos <daniel00741@gmail.com>
2017-10-19 21:31:56 -0500

    01 Creacion del Proyecto

---
+++ GigOficial/GigOficial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigOficial.MvcApplication" Language="C#" %>
---
+++ GigOficial/GigOficial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigOficial.Startup))]
+namespace GigOficial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
